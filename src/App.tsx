baeab827dd162d433e976ae2d58224f1f3139364
NidhalNaffati <nidhalnaffati@gmail.com>
2025-05-09 07:44:56 +0100

    refactor: Used the AppRoutes component for the routing instead of repeating it again the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,22 @@ import {Toaster} from "@/components/ui/toaster";
 import {Toaster as Sonner} from "@/components/ui/sonner";
 import {TooltipProvider} from "@/components/ui/tooltip";
 
-import Index from "@/pages/Index";
-import Home from "@/pages/Home";
-import Settings from "@/pages/Settings";
-import Transcription from "@/pages/Transcription";
-import Models from "@/pages/Models";
-import Live from "@/pages/LiveSpeak";
-import WebSpeech from "@/pages/WebSpeech.tsx";
-import NotFound from "@/pages/NotFound";
-
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {ThemeProvider} from "./components/ThemeProvider";
+import AppRoutes from "@/route/AppRoutes.tsx";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <ThemeProvider defaultTheme="dark">
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster/>
-        <Sonner/>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/settings" element={<Settings/>}/>
-            <Route path="/webspeech" element={<WebSpeech/>}/>
-            <Route path="/transcription" element={<Transcription/>}/>
-            <Route path="/models" element={<Models/>}/>
-            <Route path="/live" element={<Live/>}/>
-            <Route path="*" element={<NotFound/>}/>
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </ThemeProvider>
+    <ThemeProvider defaultTheme="dark">
+        <QueryClientProvider client={queryClient}>
+            <TooltipProvider>
+                <Toaster/>
+                <Sonner/>
+                <AppRoutes/>
+            </TooltipProvider>
+        </QueryClientProvider>
+    </ThemeProvider>
 );
 
 export default App;
--- src/route/AppRoutes.tsx
+++ src/route/AppRoutes.tsx
@@ -1,22 +1,30 @@
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../pages/Home.tsx";
 import Settings from "../pages/Settings.tsx";
 import Transcription from "../pages/Transcription.tsx";
 import Models from "../pages/Models.tsx";
 import Live from "../pages/LiveSpeak.tsx"
+import Index from "@/pages/Index.tsx";
+import WebSpeech from "@/pages/WebSpeech.tsx";
+import NotFound from "@/pages/NotFound.tsx";
 
 function AppRoutes() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" Component={Home}/>
-        <Route path="/settings" Component={Settings}/>
-        <Route path="/transcription" Component={Transcription}/>
-        <Route path="/models" Component={Models}/>
-        <Route path="/live" Component={Live}/>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/settings" element={<Settings/>}/>
+                    <Route path="/webspeech" element={<WebSpeech/>}/>
+                    <Route path="/transcription" element={<Transcription/>}/>
+                    <Route path="/models" element={<Models/>}/>
+                    <Route path="/live" element={<Live/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default AppRoutes;
